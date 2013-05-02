a8459616c6a7c61db8ded467936a65739f43bf91
Kevin Shen <kwshen@gmail.com>
2013-05-02 04:33:56 -0400

    borough labels

--- js/render_map.js
+++ js/render_map.js
@@ -1,33 +1,34 @@
+/*
 var width = 754,
     height = 700;
-
+*/
+var width = 600, height = 600;
+var labelCoords = {
+	"Staten Island": "(94, 416)",
+	"Bronx": "(245, 115)",
+	"Manhattan": "(170, 220)",
+	"Queens": "(300, 240)",
+	"Brooklyn": "(230, 330)",
+}
+/*
+// Places into parallax scrolling
+var svg = d3.select("#end").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+*/
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
-
 
 d3.json("./data/ny_coast.json", function(error, nyc) {
 	var boroughs_water = topojson.object(nyc, nyc.objects.boroughs_water);	
 	var boroughs_land = topojson.object(nyc, nyc.objects.boroughs_land);	
 	var states = topojson.object(nyc, nyc.objects.states);	
 
-	/*
-	var projection = d3.geo.mercator();
-		.scale(250)
-		.translate([width/2, height/2]);
-	*/	
-
 	var projection = d3.geo.albers()
-	    .scale(55000)
+	    .scale(58000)
 	    .translate([width / 2, height / 2])
-	    .center([24.19, 40.7]);
-
-	    /*
-	    .scale(1000)
-	    .translate([width / 2, height / 2]);
-	    .rotate([4.4, 0])
-	    .parallels([50, 60]);
-	    */
+	    .center([24.12, 40.67]);
 
 	// Draw the projection
 	var path = d3.geo.path()
@@ -43,14 +44,12 @@ d3.json("./data/ny_coast.json", function(error, nyc) {
 		.enter().append("path")
 		.attr("class", 
 			function(d) {
-				console.log(d);
 				return "state " 
 				+ d.properties.name.replace(" ", "-");
 			})
 		.attr("d", path);
 
 	// Draw the water boundaries
-	/*
 	svg.append("path")
 		.datum(boroughs_water)
 		.attr("d", path);
@@ -64,7 +63,6 @@ d3.json("./data/ny_coast.json", function(error, nyc) {
 					+ d.properties.name.replace(" ", "-") + "-water");
 			})
 		.attr("d", path);
-	*/
 
 	// Draw the land boundaries
 	svg.append("path")
@@ -80,12 +78,17 @@ d3.json("./data/ny_coast.json", function(error, nyc) {
 				+ d.properties.name.replace(" ", "-") + "-land";
 			})
 		.attr("d", path);
+	var labeldata = topojson.object(nyc, nyc.objects.boroughs_land).geometries;
+	console.log(labeldata);
+	svg.selectAll(".place-label")
+		.data(labeldata)
+		.enter().append("text")
+		.attr("class", "place-label")
+		.attr("transform", function(d) { 
+			return ("translate" 
+				+ labelCoords[d.properties.name.replace("-", " ")]); 
+			})
+		.attr("dy", ".35em")
+		.text(function(d) { return d.properties.name; });
 
-	
-	/*
-	svg.append("path")
-		.datum(topojson.mesh(nyc, nyc.objects.subunits, function(a, b) { return a !== b && a.id !== "IRL"; }))
-		.attr("d", path)
-		.attr("class", "subunit-boundary");
-	*/
 });
